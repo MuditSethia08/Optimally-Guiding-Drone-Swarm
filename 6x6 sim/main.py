2f25267ac7019c0e52cbd4b5e4b95ff27430d016
Kaushik Chavali <106115357+Kau108@users.noreply.github.com>
2024-05-01 15:48:55 +0530

    Update main.py
    screen recorder

--- 6x6 sim/main.py
+++ 6x6 sim/main.py
@@ -4,6 +4,8 @@ import math
 from environmentTS import CombatEnvironment
 from environmentTS import num_d
 import numpy as np
+from pygame_screen_record import ScreenRecorder
+
 def parse_strat(file_path,index):
     with open(file_path, 'r') as file:
         lines = file.readlines()
@@ -12,14 +14,15 @@ def parse_strat(file_path,index):
         A = int(columns[1])
         a = np.zeros((num_d,1))
         for i in range(num_d):
-            a[i] = math.floor(int(A%4))
-            A = math.floor(int(A/4))
+            a[i] = math.floor(int(A%8))
+            A = math.floor(int(A/8))
     return a
 
 def main():
     env = CombatEnvironment()
     running = True
     clock = pygame.time.Clock()
+    recorder = ScreenRecorder(60).start_rec()
 
     while running:
         for event in pygame.event.get():
@@ -30,16 +33,19 @@ def main():
         if(current_state == 1):
             print('Game Over')
             break
-        action = parse_strat('strategy_TS.txt',current_state)
-        print(action)
+        action = parse_strat('strategy_K.txt',current_state)
+        # print(current_state,action)
 
-        input("Press Any Key For Next State")
+        # input("Press Any Key For Next State")
 
         env.step(action)
-        clock.tick(2)  # Control simulation speed, 2 steps per second
+        clock.tick(1)  # Control simulation speed, 2 steps per second
 
         # Add additional game logic (e.g., user input) as needed
+    clock.tick(1)
 
+    recorder.stop_rec().get_single_recording().save(('r','mp4'))
+    clock.tick(1)
     pygame.quit()
 
 if __name__ == "__main__":
