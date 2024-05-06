0307daf08ff238944351ab3387740240c3526ca5
Kaushik Chavali <106115357+Kau108@users.noreply.github.com>
2024-05-06 23:45:33 +0530

    Delete Prototype directory

--- Prototype/defense.py
+++
@@ -1,5 +0,0 @@
-class DefenseSystem:
-    def __init__(self, position, defense_type, health):
-        self.position = position
-        self.defense_type = defense_type
-        self.health = health
--- Prototype/transition.py
+++
@@ -1,3 +0,0 @@
-# this is a file that governs the phyiscal transition of the game
-
-from agent imp
